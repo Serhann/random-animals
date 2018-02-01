ba44f2bca563364e28d018cd5229f97f0a4b25c7
Serhann <serhanpw@gmail.com>
2018-02-01 14:18:41 +0300

    add doge

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const snekfetch = require('snekfetch');
 class randomAnimal {
 
 	/**
-	 * It returns a cat :)
+	 * It returns a random cat
 	 */
 	cat() {
 		try {
 			var { body } = await snekfetch
 			.get('http://random.cat/meow');
-        		return 'body.file';
+        		return body.file;
 		} catch (err) {
 			throw 'Error: ' + err.message;
 		}
 	}
-	
+
+	/**
+	 * It returns a random dog
+	 */
+	dog() {
+		try {
+			const { body } = await snekfetch
+				.get('https://random.dog/woof.json');
+                	return body.url;
+		} catch (err) {
+			throw 'Error: ' + err.message;
+		}
+	}
 }
 
 module.exports = new randomAnimal();
